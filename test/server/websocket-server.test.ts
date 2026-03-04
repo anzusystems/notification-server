6ee82bd4db20fb8c3f72389f590a3181741d379b
Andrej Hudec <pulzarraider2@gmail.com>
2026-03-04 15:28:03 +0100

    Fix CI

--- test/server/websocket-server.test.ts
+++ test/server/websocket-server.test.ts
@@ -59,8 +59,6 @@ describe('server/websocket-server test', () => {
     })
     const userConnection = userConnections.getAllForUser('123')
     expect(userConnection.size).toEqual(1)
-    const socket = userConnection.values().next().value
-    expect(socket).toBeDefined()
-    socket!.send('hello')
+    userConnection.values().next().value?.send('hello')
   })
 })
